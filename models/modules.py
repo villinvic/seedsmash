3efaa912a6f81fe4d2c29d07099b1273f97568d0
villinvic <vvillin@hotmail.fr>
2024-10-27 23:05:12 +0100

    2

--- models/modules.py
+++ models/modules.py
@@ -129,10 +129,8 @@ class ResItem(snt.Module):
         sample_embedding = tf.cast(self.embedder(self, sample), tf.float32)
         residual += self.decoder(sample_embedding)
         sample = tf.squeeze(sample)
-        if tf.rank(sample) == 0:
-            sample = tf.expand_dims(sample, axis=0)
 
-        return residual, logits, sample, sample_embedding
+        return residual, logits, sample[0, 0], sample_embedding
 
 
 
