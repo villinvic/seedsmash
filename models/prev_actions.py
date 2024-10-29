9d2c568d87aea133dd4eebb41c3c63380c0ce6cd
villinvic <vvillin@hotmail.fr>
2024-10-29 18:33:53 +0100

    action stacking

--- models/prev_actions.py
+++ models/prev_actions.py
@@ -227,7 +227,6 @@ class ActionStacking(BaseModel):
         true_player_obs = self_true + opp_true
 
         if single_obs:
-            print(player_obs)
             player_obs = [add_batch_time_dimensions(tf.concat(player_obs, axis=0))]
             true_player_obs = [add_batch_time_dimensions(tf.concat(true_player_obs, axis=0))]
 
